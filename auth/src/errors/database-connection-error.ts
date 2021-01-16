4ba8be8227430f8e8ec8f5ee7041b83bc698d8f8
goegrasu <bogdanlr1985@gmail.com>
2021-01-16 19:37:05 +0200

    finishing error handling

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,10 +1,22 @@
-export class DatabaseConnectionError extends Error {
+import {CustomError} from './custom-error';
 
+export class DatabaseConnectionError extends CustomError {
+    statusCode = 500;
     reason = 'Error connection to database';
 
     constructor() {
-        super();
+        super('Error connecting to database');
 
         Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
+
+    serializeErrors() {
+
+        return [
+            {
+                message: this.reason,
+
+            }
+        ];
     }
 }
--- auth/src/middlewares/error-handlers.ts
+++ auth/src/middlewares/error-handlers.ts
@@ -1,7 +1,6 @@
 
 import { Request, Response, NextFunction } from 'express';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (
     err: Error, 
@@ -10,25 +9,9 @@ export const errorHandler = (
     next: NextFunction
 ) => {
 
-    if (err instanceof RequestValidationError) {
-        const formattedErrors = err.errors.map((error) => {
-            return {
-                message: error.msg,
-                field: error.param
-            }
-        });
-        return res.status(400).send({errors: formattedErrors});
-    }
-
-    if (err instanceof DatabaseConnectionError) {
-        return res.status(500).send({ 
-            errors: [
-                {
-                    message: err.reason
-                }
-            ]
-        });
-    }
+    if (err instanceof CustomError) {
+        return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+    }    
 
     res.status(400).send({
         errors: [{message: 'Something went wrong'}]
